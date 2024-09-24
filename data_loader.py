94f63644863f0ebc030ebb8a98f38b5e41918b9c
WanyaKruk <wanya.kruk@gmail.com>
2024-09-24 17:44:33 +0300

    Add files via upload

--- data_loader.py
+++ data_loader.py
@@ -1,7 +1,9 @@
 import pandas as pd
 class DataLoader:
   def load_datacsv(file_path):
-    return pd.read_csv(file_path)
+    data = pd.read_csv(file_path)
+    return data
   
   def load_dataxcl(file_path):
-    return pd.read_excel(file_path)
+    data = pd.read_excel(file_path)
+    return data
