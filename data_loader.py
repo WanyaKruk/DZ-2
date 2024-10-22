ee541191c2c257ddeccc79d92bfd1eb851fc3d4c
WanyaKruk <wanya.kruk@gmail.com>
2024-10-22 17:43:49 +0300

    Add files via upload

--- data_loader.py
+++ data_loader.py
@@ -1,9 +1,9 @@
 import pandas as pd
-class DataLoader:
-  def load_datacsv(file_path):
-    data = pd.read_csv(file_path)
-    return data
-  
-  def load_dataxcl(file_path):
-    data = pd.read_excel(file_path)
-    return data
+
+def load_csv(file_path):
+        data = pd.read_csv(file_path)
+        return data
+
+def load_excel(file_path):
+        data = pd.read_excel(file_path)
+        return data
--- empty_cell.py
+++ empty_cell.py
@@ -1,13 +1,27 @@
 import pandas as pd
-class FindEmptyCell: 
-    def __init__(self, df):
-        self.df = df
+
+class DataFrameAnalyzer:
+  def __init__(self, data):
+   self.data = data
+
+  def missing_values_report(self):
+   missing_values = self.df.isnull().sum()
+   missing_percentage = (missing_values / len(self.df)) * 100
+   report = pd.DataFrame({
+'Missing Values': missing_values,
+'Percentage': missing_percentage
+})
+   return report
+
+  def fill_missing_values(self, method='mean'):
+   if method == 'mean':
+    self.df.fillna(self.df.mean(), inplace=True)
+   elif method == 'median':
+    self.df.fillna(self.df.median(), inplace=True)
+   elif method == 'mode':
+    self.df.fillna(self.df.mode().iloc[0], inplace=True)
+   else:
+    raise ValueError("Метод должен быть 'mean', 'median', or 'mode'")
+
+
     
-    def count_empty_cell(self):
-        count_cell = self.df.isnull().sum()
-        return count_cell
-    
-    def generate_report(self):
-        count_cell = self.count_empty_cell()
-        report = pd.DataFrame()
-        return report
